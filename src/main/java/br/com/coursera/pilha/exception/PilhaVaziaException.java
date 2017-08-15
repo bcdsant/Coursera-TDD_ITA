537485c658b699ccaf59d001e8af2d59f08650e7
Bruno <Bruno@Bruno-Note>
2017-08-15 16:35:51 -0300

    Removendo arquivos desnecessários

--- src/main/java/br/com/coursera/pilha/exception/PilhaVaziaException.java
+++
@@ -1,9 +0,0 @@
-package br.com.coursera.pilha.exception;
-
-public class PilhaVaziaException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-	
-	public PilhaVaziaException(String msg) {
-		super(msg);
-	}
-}
